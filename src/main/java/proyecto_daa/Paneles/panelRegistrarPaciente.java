c0bf94b9626bd25650c76c93831429f8b60dbd4c
Eduardo <farid.merino1673@gmail.com>
2024-05-19 13:18:18 -0500

    pruebitaParaAsegurarmeDeNoDañarAlgo

--- src/main/java/proyecto_daa/Paneles/panelRegistrarPaciente.java
+++ src/main/java/proyecto_daa/Paneles/panelRegistrarPaciente.java
@@ -162,6 +162,7 @@ public class panelRegistrarPaciente extends javax.swing.JFrame {
 
     private void txtNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNombreActionPerformed
         // TODO add your handling code here:
+        //pruebita nomas
     }//GEN-LAST:event_txtNombreActionPerformed
 
     private void txtApellidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtApellidoActionPerformed
